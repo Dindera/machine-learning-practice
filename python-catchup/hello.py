560189c2cf346ad9618cbdec8dae62de18591cbf
Chidera Ozoh <Chideraozoh@icloud.com>
2020-11-19 11:00:10 +0000

    python update

---
+++ python-catchup/hello.py
@@ -0,0 +1,21 @@
+message = "hello world!"
+print(message)
+
+name = "dera ozoh"
+print(name.title())
+
+first_name = "dera"
+last_name = "ozoh"
+
+full_name = f'{first_name} {last_name}'
+print(full_name)
+print(f"Welcome {full_name.title()}")
+
+def getArea(base, height):
+    area = (base * height) / 2
+
+    return area
+
+print(getArea(4, 5))
+
+
---
+++ python-catchup/name_space.py
@@ -0,0 +1,31 @@
+# name = "Geeklady"
+
+# message = f'Hello {name}, would you like to learn some Python today?'
+
+# person_name = "Chidera Ozoh"
+
+# person_name2 = " \nDera "
+
+# famous_person = "Albert Einstein"
+
+# famous_quote1 = f'{famous_person} once said, “A person who never made a mistake never tried anything new.”'
+
+# print(message)
+# print(f'{person_name.lower()} {person_name.upper()} {person_name.title()}')
+# print(famous_quote1)
+# print(person_name2)
+# print(person_name2.lstrip())
+# print(person_name2.rstrip())
+# print(person_name2.strip())
+
+# /********** Integers **********/
+
+
+# create an exponent
+exponents = 3 **  2
+
+universe_age = 14_000_000
+
+print(universe_age)
+
+print(exponents)
